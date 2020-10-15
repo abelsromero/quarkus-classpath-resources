602742d4ae1c616359aa2f261cd2c15b543ff3fb
Abel Salgado Romero <abel.salgado@weekendesk.fr>
2020-10-15 18:51:20 +0200

    Only Java running

---
+++ src/main/java/org/acme/resteasy/ResourceResource.java
@@ -0,0 +1,35 @@
+package org.acme.resteasy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+@Path("/resource")
+public class ResourceResource {
+
+    @GET
+    @Path("file")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloFile() throws IOException {
+        log.info("/file");
+        InputStream resourceAsStream = ResourceResource.class.getResourceAsStream("/myfile.properties");
+        return IOUtils.toString(resourceAsStream, StandardCharsets.UTF_8);
+    }
+
+    @GET
+    @Path("string")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloString() {
+        log.info("/string");
+        return "string";
+    }
+
+}
